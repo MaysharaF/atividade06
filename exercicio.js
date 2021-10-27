2fb6bbe1b0599601959092f0bb42ee8170d7e303
MaysharaF <mayshara@alunos.utfpr.edu.br>
2021-10-26 22:41:28 -0300

    Resolvendo exercicio para retornar menor id

--- exercicio.js
+++ exercicio.js
@@ -6,4 +6,17 @@ const buscaGerente = () => {
 console.log(buscaGerente())
 
 // - 2º commit: Use o reduce() para retornar o objeto com o menor id
+const retornaMenorId = () => {
+
+    return Funcionarios.reduce((valorAnterior, valorAtual) => {
+        if (valorAtual.id < valorAnterior.id) {
+
+            valorAnterior = valorAtual
+        }
+        return valorAnterior
+    }, Funcionarios[0])
+
+}
+console.log(retornaMenorId())
+
 // - 3º commit: Use o map() para colocar todos os nomes no formato UpperCase.
